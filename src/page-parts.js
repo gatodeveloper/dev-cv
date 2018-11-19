28c5c71253b067e90ab51f84a979e9cc7055f8cf
Eyal Perry <e.perry.pa@gmail.com>
2018-11-19 15:28:52 +0200

    - support for multi page cv. - browser page width now reflects print media width. - education item now renders the tech stack data. - website reloaded on cv.json change. - project folder re-ordering. - better support for dynamic component rendering via the page-parts module.

---
+++ src/page-parts.js
@@ -0,0 +1,13 @@
+import {Label} from 'Common';
+import Brief from "./brief";
+import Education from "./education";
+import Experience from "./experience";
+
+const parts = {
+    "label": Label,
+    'brief': Brief,
+    'education': Education,
+    'experience': Experience
+};
+
+export default parts;
